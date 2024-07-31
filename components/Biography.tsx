054bdc809a13b3a216ade9e61bf3b93b2791cd51
fazil108 <onlinksolutionsinfo@gmail.com>
2024-08-01 01:03:46 +0530

    comit

--- components/Biography.tsx
+++ components/Biography.tsx
@@ -23,7 +23,7 @@ const Biography = () => {
           breakpoints={{
             328: {
               slidesPerView: 2,
-              spaceBetween: 40,
+              spaceBetween: 20,
             },
             768: {
               slidesPerView: 3,
